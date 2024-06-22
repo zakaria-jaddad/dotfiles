e0cd2d9de270d3b76a405bcfd425538b4ffbe543
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-06-22 22:32:15 +0100

    Changed bufferline from tabs to buffers and add options

--- .config/nvim/lua/zakaria/plugins/bufferline.lua
+++ .config/nvim/lua/zakaria/plugins/bufferline.lua
@@ -2,10 +2,27 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "",
-    },
-  },
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "buffers",
+        diagnostics = "nvim_lsp",
+        separator_style = "thin", -- | "slope" | "thin" | "slant",
+        indicator = {
+          icon = "▎", -- this should be omitted if indicator style is not 'icon'
+          style = "icon",
+        },
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "center",
+            highlight = "Directory",
+            separator = true,
+          },
+        },
+      },
+    })
+  end,
 }
